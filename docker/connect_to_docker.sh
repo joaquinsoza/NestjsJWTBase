a4afb808a1c36bd5b5144c1e752c48fe5b938444
coderipper <hello@joaquinsoza.dev>
2024-01-30 23:45:03 +0100

    docker compose

--- docker/connect_to_docker.sh
+++ docker/connect_to_docker.sh
@@ -1,2 +1,3 @@
 echo "Opening bash console"
 docker exec --tty --interactive nestjsjwtbase-backend-1 bash
+# docker exec --tty --interactive nestjsjwtbase-backend-1 /bin/sh
--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-yarn install
-
-# yarn prisma migrate deploy
-
-# yarn prisma generate
-
-# exec yarn start:dev
